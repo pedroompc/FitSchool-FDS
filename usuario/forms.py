7866cfe658392a1b4a113b0c75e34bed1d9eb71f
arthursindeaux <arthursindeaux1@gmail.com>
2025-09-22 19:30:23 -0300

    ajustando css criar conta

--- usuario/forms.py
+++ usuario/forms.py
@@ -1,9 +1,25 @@
+# usuario/forms.py
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class RegistroForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Seu e-mail'})
+    )
+    username = forms.CharField(
+        label='Nome de usuário',
+        widget=forms.TextInput(attrs={'placeholder': 'Seu usuário'})
+    )
+    password1 = forms.CharField(
+        label='Senha',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Crie uma senha'})
+    )
+    password2 = forms.CharField(
+        label='Confirmar senha',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repita sua senha'})
+    )
 
     class Meta:
         model = User
