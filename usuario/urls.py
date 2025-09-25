aa278cd545d41a30cbcfbdd3191fe50f8bc1d9cb
arthursindeaux <arthursindeaux1@gmail.com>
2025-09-25 09:14:39 -0300

    Finalizando Home

--- usuario/urls.py
+++ usuario/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('registrar/', views.registrar, name='registrar'),
+    path("login/", views.login_user, name="login"),
     path('menu/', views.menu_view, name='menu'),
     path('frequencia/', views.frequencia, name='frequencia'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render, redirect
 from .forms import RegistroForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")  
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("menu")  
+        else:
+            messages.error(request, "Usuário ou senha inválidos")
+            return redirect("home")  
+
+    return redirect("home")
 
 def registrar(request):
     if request.method == "POST":
